460e8ba406f88caac7192eb0a89fd20f96856526
Chris <moostemaster1@gmail.com>
2018-10-27 13:08:25 -0400

    created Main Screen and Login Screens

--- AODesktop/src/main/java/com/ao/desktop/Main.java
+++ AODesktop/src/main/java/com/ao/desktop/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/main_screen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/Login_Screen.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 493, 357));
         primaryStage.show();
     }
 
--- AODesktop/src/main/java/com/ao/desktop/controllers/Controller.java
+++ AODesktop/src/main/java/com/ao/desktop/controllers/Controller.java
@@ -1,4 +1,74 @@
 package com.ao.desktop.controllers;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Node;
+
+
+public class Controller implements Initializable
+{
+    @FXML
+    private Button signInButton;
+    @FXML
+    private TextField UserNameInput;
+    @FXML
+    private TextField PasswordInput;
+    @FXML
+    private Label errorSignIn;
+
+    private String tempUser;
+    private String tempPass;
+    @FXML
+    public void signIn(ActionEvent e)
+    {
+         String teachUser = "Wenjia";
+         String teachPass = "Li";
+        tempUser= UserNameInput.getText();
+        tempPass = PasswordInput.getText();
+
+        if(!tempUser.equals(teachUser) || !tempPass.equals(teachPass))
+        {
+            errorSignIn.setText("Wrong username or password");
+        }
+        else
+        {
+            try {
+                Parent Main_Screen_Parent = FXMLLoader.load(getClass().getResource("/Main_Screen.fxml"));
+                Scene Main_Screen_Scene = new Scene(Main_Screen_Parent);
+                Stage Main_Stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+                Main_Stage.setScene(Main_Screen_Scene);
+                Main_Stage.show();
+            }
+            catch(IOException ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+
+    }
+
+
+
+
+
 }
