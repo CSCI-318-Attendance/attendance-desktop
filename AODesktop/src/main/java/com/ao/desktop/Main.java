318e643f5eeac5080da74ae81115df0f1be799b3
Chris <moostemaster1@gmail.com>
2018-11-08 13:51:54 -0500

    LaptopAppUpdate

--- AODesktop/src/main/java/com/ao/desktop/Main.java
+++ AODesktop/src/main/java/com/ao/desktop/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/Login_Screen.fxml"));
-        primaryStage.setTitle("Quick Attendance");
+        primaryStage.setTitle("Attendance Overview");
         primaryStage.setScene(new Scene(root, 493, 357));
         primaryStage.show();
     }
